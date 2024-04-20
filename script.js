73823f7c8a02daf1d5f3e81c28b9d49ab6fade29
Poornima2118 <167555306+Poornima2118@users.noreply.github.com>
2024-04-20 12:40:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// var typed = new Typed(".typing", {
+//     strings:["Web Designer","Web developer","Graphic Designer","YouTuber"],
+//     typeSpeed: 100,
+//     BackSpeed: 60,
+//     loop: true
+// })
